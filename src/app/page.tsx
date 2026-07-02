6199b486d0ddadacdebbe8e40c4760a14f2b6b5c
Rohan <rohanshikhare410@gmail.com>
2026-07-02 16:04:31 +0530

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Page() {
                 subtitle={work.title}
                 href={work.href}
                 badges={work.badges}
-                period={`${work.start} - ${work.end ?? "Present"}`}
+                period={`${work.start} - ${"end" in work ? work.end ?? "Present" : "Present"}`}
                 description={work.description}
               />
             </BlurFade>
